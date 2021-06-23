616dd0bfddaae60f9b16be137735fbd9860692dc
EnigmaticKAUSAR <60642871+IamKausar@users.noreply.github.com>
2021-06-23 13:23:00 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about Component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolores aperiam error neque totam natus dolorem nisi odit consequuntur sit quidem modi 
+                praesentium ipsam, explicabo necessitatibus earum eum culpa sed quod autem eveniet odio.</p> 
+        </div>
+    )
+}
